9afbb3a97a70644bfa97263c8fcffaae816b770a
Olive <olivelliott48@gmail.com>
2022-07-01 17:01:05 -0400

    Create post route to add a voter to the database

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -38,6 +38,32 @@ router.get('/voter/:id', (req, res) => {
     });
 });
 
+// * Create a single voter
+router.post('/voter', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+    if(errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?, ?, ?)`;
+    const params = [body.first_name, body.last_name, body.email];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+
+// *
+
 module.exports = router;
 
 
