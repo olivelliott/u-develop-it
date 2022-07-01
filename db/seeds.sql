ad70fc3d350c5535645ea279b56f576aa2aa6fb7
Olive <olivelliott48@gmail.com>
2022-07-01 13:32:21 -0400

    Create and populate parties table

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,3 +10,10 @@ VALUES
   ('Montague', 'Summers', 1),
   ('Octavia', 'Butler', 1),
   ('Unica', 'Zurn', 1);
+
+
+INSERT INTO parties (name, description)
+VALUES
+  ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, including a new kitchen sink.'),
+  ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds.'),
+  ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
