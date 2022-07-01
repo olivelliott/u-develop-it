b50588a2f672464baec08f7efc5c672fd5dadb17
Olive <olivelliott48@gmail.com>
2022-06-30 21:46:09 -0400

    Build database calls to delete and create candidates

--- server.js
+++ server.js
@@ -1,12 +1,52 @@
 const express = require('express');
+const mysql = require('mysql2');
+const nodemon = require('nodemon');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+  {
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "election",
+  },
+  console.log("Connected to election database")
+);
+
+// GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(row);
+});
+
+//* Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a new candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+            VALUES (?, ?, ?, ?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
 
 
+//  Catch all route for error handling
 app.use((req, re) => {
     res.status(404).end();
 })
