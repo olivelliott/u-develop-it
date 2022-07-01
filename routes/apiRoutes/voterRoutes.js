37018928b378b0403e7429e5d647cefb05b5904c
Olive <olivelliott48@gmail.com>
2022-07-01 17:08:48 -0400

    Add delete route to delete voter from database

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -92,6 +92,28 @@ router.post('/voter/:id', (req, res) => {
     });
 });
 
+
+// * Delete voter from database
+router.delete("/voter/:id", (req, res) => {
+    const sql = `DELETE FROM voters WHERE id = ?`;
+
+    db.query(sql, req.params.id, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: "Voter not found",
+            });
+        } else {
+            res.json({
+                message: "deleted",
+                changes: result.affectedRows,
+                id: req.params.id,
+            });
+        }
+    });
+});
+
 module.exports = router;
 
 
